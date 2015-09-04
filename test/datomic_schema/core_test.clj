9b382d8355e067a4419df31456420c6a9238b421
Thomas Crowley <crowley@kibu.com.au>
2015-09-04 15:51:04 +1000

    Initial commit

---
+++ test/datomic_schema/core_test.clj
@@ -0,0 +1,7 @@
+(ns datomic-schema.core-test
+  (:require [clojure.test :refer :all]
+            [datomic-schema.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
