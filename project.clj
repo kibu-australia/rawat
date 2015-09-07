d4267a58e27dfabac742dcc8c2ba7cea77a3981a
Thomas Crowley <crowley@kibu.com.au>
2015-09-07 10:26:00 +1000

    Release 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rawat "0.1.0-SNAPSHOT"
+(defproject kibu/rawat "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
