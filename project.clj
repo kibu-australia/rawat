1f19dca1179c74855f6d8b574cae3572f490776d
Thomas Crowley <crowley@kibu.com.au>
2015-10-26 21:35:08 +1100

    Bump to 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kibu/rawat "0.1.9"
+(defproject kibu/rawat "0.2.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
