a88c19531e1dd306cb4097dfaceb4914fa66c2bc
Thomas Crowley <crowley@kibu.com.au>
2016-05-20 16:51:54 +1000

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.match "0.3.0-alpha4"]
-                 [prismatic/schema "1.0.1"]
-                 [com.datomic/datomic-free "0.9.5206"]]
+                 [prismatic/schema "1.0.1" :scope "provided"]
+                 [com.datomic/datomic-free "0.9.5206" :scope "provided"]]
   :aliases {"deploy" ["do" "clean," "deploy" "clojars"]}
   :lein-release {:deploy-via :shell
                  :shell ["lein" "deploy"]}
